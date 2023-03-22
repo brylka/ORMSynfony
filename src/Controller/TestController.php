62701a02bdca0d25e97d42ff194b865e72c686e6
Bartosz Bryniarski <brylka@jelenia.com>
2023-03-23 00:05:14 +0100

    Dodanie i18n

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,14 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
-    public function index(): Response
+    public function indexTest(TranslatorInterface $translator): Response
+    {
+        $translatedText = $translator->trans('hello.world');
+        return new Response($translatedText);
+    }
+
+    #[Route('/test-view', name: 'app_test_view')]
+    public function indexTestView(): Response
     {
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
